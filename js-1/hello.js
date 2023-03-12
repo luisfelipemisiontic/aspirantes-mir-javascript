b552b6042c560a3c593fc6ca3a9422c57de6cd1b
luisfelipemisiontic <luisfeliperomerolozano@gmail.com>
2023-03-11 20:55:39 -0500

    solucion a los ejecicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,9 @@
+function hola(name){
+    const frase = "Hola" + " " + name + "!"
+
+    return frase
+}
+
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,10 @@
+function suma(sum){
+    const sumatoria = sum*(sum+1)/2;
+
+    return sumatoria
+}
+
+// código de prueba
+console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)) // 55
+console.log(suma(15)) // 120
